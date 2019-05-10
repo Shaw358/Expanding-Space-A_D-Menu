7aad3f65342db28057f1043a087f77dc2fc22dee
General Grievous (Lucas) <42550065+Shaw358@users.noreply.github.com>
2019-05-10 09:49:31 +0200

    Initial commit

---
+++ Assets/Scripts/MenuSelector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSelector : MonoBehaviour
+{
+
+    public void Start_Game()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
